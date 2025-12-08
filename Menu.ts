ea7060b6482ecf47788364cd8304ea58eb57cf61
Robson Valentim <rgvalentim@outlook.com>
2025-12-08 17:30:37 -0300

    Adicionada a classe conta

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,18 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from './src/model/Conta';
 
 export function main() {
 
     let opcao: number;
+
+    // Objeto da Classe Conta (Teste)
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
 
     while (true) {
 
