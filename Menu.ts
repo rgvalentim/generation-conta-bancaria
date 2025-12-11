89306ca942c98685a51d6881c0b388383e2db0ab
Robson Valentim <rgvalentim@outlook.com>
2025-12-11 15:12:53 -0300

    Finalizacao do programa

--- Menu.ts
+++ Menu.ts
@@ -2,11 +2,34 @@ import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { ContaCorrente } from "./src/model/ContaCorrente";
 import { ContaPoupanca } from "./src/model/ContaPoupanca";
+import { ContaController } from "./src/controller/ContaController";
 
 export function main() {
+    //Instância da Classe ContaController
+    let contas: ContaController = new ContaController();
 
-    let opcao: number;
+    //Variáveis Auxiliares
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario, valor, numeroDestino: number;
+    let titular: string;
+    const tiposContas = ["Conta Corrente", "Conta Poupanca"]
 
+    console.log("\nCriar Contas\n");
+
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
+
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
+
+    
     /* Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
@@ -14,10 +37,9 @@ export function main() {
     conta.visualizar();
     conta.depositar(5000);
     conta.visualizar(); 
-
     No momento que tornou a classe Conta abstrata ela não pode mais ser instanciada*/
 
-    //Objeto da classe ContaCorrente
+    /*Objeto da classe ContaCorrente
     const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
     contacorrente.visualizar();
     contacorrente.sacar(2000);
@@ -31,7 +53,7 @@ export function main() {
     contapoupanca.sacar(200);
     contapoupanca.visualizar();
     contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+    contapoupanca.visualizar(); */
 
 
 
@@ -73,48 +95,116 @@ export function main() {
             case 1:
                 console.log(colors.fg.whitestrong, 
                     "\n\nCriar Conta\n\n", colors.reset);
+
+                agencia = readlinesync.questionInt("\nDigite o numero da agencia: ");
+                titular = readlinesync.question("\nDigite o nome do Titular da Conta: ");
+                tipo = readlinesync.keyInSelect(tiposContas, "Digite o tipo da conta: ", {cancel: false}) + 1;
+                saldo = readlinesync.questionFloat("Digite o Saldo da Conta (R$): ");
+
+                switch (tipo) {
+                    case 1:
+                        limite = readlinesync.questionFloat("Digite o Limite da Conta (R$): ");
+                        contas.cadastrar(
+                            new ContaCorrente(contas.gerarNumero(), agencia,
+                        tipo, titular, saldo, limite));
+                        break;
+                    case 2:
+                        aniversario = readlinesync.questionInt("Digite o dia do aniversario da conta Poupanca: ");
+                        contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia,
+                    tipo, titular, saldo, aniversario));
+                    break;
+                }
                 
                 keyPress()
                 break;
             case 2:
                 console.log(colors.fg.whitestrong, 
                     "\n\nListar todas as Contas\n\n", colors.reset);
+                contas.listarTodas();
 
                 keyPress()
                 break;
             case 3:
                 console.log(colors.fg.whitestrong, 
                     "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+                numero = readlinesync.questionInt("Digite o numero da Conta: ");
+                contas.procurarPorNumero(numero);
 
                 keyPress()
                 break;
             case 4:
-                console.log(colors.fg.whitestrong, 
-                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
+                numero = readlinesync.questionInt("Digite o numero da Conta: ");
+
+                let conta = contas.buscarNoArray(numero);
+
+                if (conta != null) {
+
+                    agencia = readlinesync.questionInt("Digite o Numero da agencia: ");
+                    titular = readlinesync.question("Digite o Nome do Titular da conta: ");
+
+                    tipo = conta.tipo;
+
+                    saldo = readlinesync.questionFloat("\nDigite o Saldo da conta (R$): ");
+
+                    switch (tipo) {
+                        case 1:
+                            limite = readlinesync.questionFloat("Digite o Limite da Conta (R$): ");
+                            contas.atualizar(
+                                new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+                            break;
+                        case 2:
+                            aniversario = readlinesync.questionInt("Digite o Dia do aniversario da Conta Poupanca: ");
+                            contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo,
+                                aniversario));
+                            break;
+                    }
+
+                } else {
+                    console.log(colors.fg.red, "\nA Conta numero: " + numero +
+                        " não foi encontrada!", colors.reset);
+                }
                 keyPress()
                 break;
             case 5:
                 console.log(colors.fg.whitestrong, 
                     "\n\nApagar uma Conta\n\n", colors.reset);
 
+                numero = readlinesync.questionInt("Digite o numero da Conta: ");
+                contas.deletar(numero);
+                
+
                 keyPress()
                 break;
             case 6:
                 console.log(colors.fg.whitestrong, 
                     "\n\nSaque\n\n", colors.reset);
+                numero = readlinesync.questionInt("Digite o numero da conta: ");
+                valor = readlinesync.questionFloat("\nDigite o valor do saque (R$): ");
+                contas.sacar(numero, valor);
 
                 keyPress()
                 break;
             case 7:
                 console.log(colors.fg.whitestrong, 
                     "\n\nDepósito\n\n", colors.reset);
+                numero = readlinesync.questionInt("Digite o numero da conta: ");
+                valor = readlinesync.questionFloat("\nDigite o valor do deposito (R$): ");
+                contas.depositar(numero, valor);
+                    
 
                 keyPress()
                 break;
             case 8:
                 console.log(colors.fg.whitestrong, 
                     "\n\nTransferência entre Contas\n\n", colors.reset);
+                
+                numero = readlinesync.questionInt("Digite o numero da conta de origem: ");
+                numeroDestino = readlinesync.questionInt("Digite o numero da conta de destino: ")
+                valor = readlinesync.questionFloat("\nDigite o valor do deposito (R$): ");
+
+                contas.transferir(numero, numeroDestino, valor);
 
                 keyPress()
                 break;
---
+++ src/controller/ContaController.ts
@@ -0,0 +1,111 @@
+import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+import { colors } from "../util/Colors";
+
+export class ContaController implements ContaRepository{
+   private listaContas: Array<Conta> = new Array<Conta>();
+   numero: number = 0;
+   procurarPorNumero(numero: number): void {
+      let buscaConta = this.buscarNoArray(numero);
+
+      if (buscaConta != null){
+         buscaConta.visualizar();
+      } else{
+         console.log(colors.fg.red, "\nA conta numero: " + numero +
+            " não foi encontrada!", colors.reset);
+         
+      }
+   }
+   listarTodas(): void {
+      for (let conta of this.listaContas){
+         conta.visualizar();
+      }
+   }
+   cadastrar(conta: Conta): void {
+      this.listaContas.push(conta);
+      console.log(colors.fg.green, "\nA conta numero: " + conta.numero + 
+         " foi criada com sucesso!", colors.reset);
+      
+   }
+   atualizar(conta: Conta): void {
+    let buscaConta = this.buscarNoArray(conta.numero);
+
+    if (buscaConta != null) {
+        this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+        console.log(colors.fg.green, "\nA Conta numero: " + conta.numero + 
+                    " foi atualizada com sucesso!", colors.reset);
+    } else
+        console.log(colors.fg.red, "\nA Conta numero: " + conta.numero + 
+                    " não foi encontrada!", colors.reset);
+}
+   deletar(numero: number): void {
+      let buscaConta = this.buscarNoArray(numero);
+
+      if (buscaConta != null) {
+         this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+         console.log(colors.fg.green,"\nA Conta numero: " + numero + 
+            " foi apagada com sucesso!", colors.reset);
+      } else
+        console.log(colors.fg.red,"\nA Conta numero: " + numero + 
+        " nao foi encontrada!", colors.reset);
+   }
+   public sacar(numero: number, valor: number): void {
+      let conta = this.buscarNoArray(numero);
+
+      if (conta != null) {
+
+         if(conta.sacar(valor) == true)
+            console.log(colors.fg.green,"\nO Saque na Conta numero: " + numero + 
+               " foi efetuado com sucesso!", colors.reset);
+
+      }else
+         console.log(colors.fg.red,"\nA Conta numero: " + numero + 
+            " não foi encontrada!", colors.reset);
+   }
+   
+   public depositar(numero: number, valor: number): void {
+      let conta = this.buscarNoArray(numero);
+
+      if (conta != null) {
+         conta.depositar(valor);
+         console.log(colors.fg.green,"\nO Deposito na Conta numero: " + numero +
+                     " foi efetuado com sucesso!", colors.reset);
+      }else
+         console.log(colors.fg.red,"\nA Conta numero: " + numero +
+                     " nao foi encontrada!", colors.reset);
+}
+
+   public transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+    let contaOrigem = this.buscarNoArray(numeroOrigem);
+    let contaDestino = this.buscarNoArray(numeroDestino);
+
+    if (contaOrigem != null && contaDestino != null) {
+        if(contaOrigem.sacar(valor) == true){
+            contaDestino.depositar(valor);
+            console.log(colors.fg.green,"\nA Transferência da Conta numero: " + numeroOrigem +
+                        " para a Conta numero: " + numeroDestino + " foi efetuada com sucesso!",
+                        colors.reset);
+        }
+
+    }else
+      console.log(colors.fg.red,"\nA Conta numero: " + numeroOrigem +
+                  " e/ou a Conta numero: " + numeroDestino + " não foram encontradas!",
+                  colors.reset);
+}
+
+   //Métodos auxiliares
+   //Gerar numero da conta
+   public gerarNumero(): number{
+      return ++ this.numero;
+   }
+
+   //Checa se a conta existe
+   public buscarNoArray(numero: number): Conta | null{
+      for (let conta of this.listaContas) {
+         if(conta.numero === numero)
+            return conta;
+      }
+      return null;
+   }
+   
+}
